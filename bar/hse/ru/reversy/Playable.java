8138791680830069a97911ed5baee23f228be5c0
GO-KPRO <98886243+GO-KPRO@users.noreply.github.com>
2022-11-28 00:43:50 +0300

    Delete Playable.java

--- bar/hse/ru/reversy/Playable.java
+++
@@ -1,9 +0,0 @@
-package bar.hse.ru.reversy;
-
-public interface Playable {
-    public void showScoreBoard();
-    public void showMaxScore();
-    public void runPvPGame();
-    public void runPvEGame();
-    public void runPvhEGame();
-}
