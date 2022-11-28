ab799a4a34b499247568c049a9e7b54b17f24142
GO-KPRO <98886243+GO-KPRO@users.noreply.github.com>
2022-11-28 16:29:56 +0300

    Add files via upload

--- bar/hse/ru/reversy/Main.java
+++ bar/hse/ru/reversy/Main.java
@@ -1,5 +1,7 @@
 package bar.hse.ru.reversy;
 
+import bar.hse.ru.reversy.runners.Menu;
+
 public class Main {
     public static void main(String[] args) {
         Menu program = Menu.getOneMenu();
