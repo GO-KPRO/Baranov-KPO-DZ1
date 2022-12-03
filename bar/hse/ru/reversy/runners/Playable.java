725731c13e393f0c3e070c928bbc1c37f3849a2d
GO-KPRO <98886243+GO-KPRO@users.noreply.github.com>
2022-12-03 14:10:06 +0300

    Add files via upload

--- bar/hse/ru/reversy/runners/Playable.java
+++ bar/hse/ru/reversy/runners/Playable.java
@@ -2,8 +2,12 @@ package bar.hse.ru.reversy.runners;
 
 public interface Playable {
     void showScoreBoard();
+
     void showMaxScore();
+
     void runPvPGame();
+
     void runPvEGame();
+
     void runPvhEGame();
 }
