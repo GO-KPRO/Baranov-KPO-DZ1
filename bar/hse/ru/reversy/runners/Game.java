e1df57bbbff8ac51ec20ac04eefeadd618a6f8b3
GO-KPRO <98886243+GO-KPRO@users.noreply.github.com>
2022-12-03 11:50:19 +0300

    Add files via upload

--- bar/hse/ru/reversy/runners/Game.java
+++ bar/hse/ru/reversy/runners/Game.java
@@ -4,24 +4,36 @@ import bar.hse.ru.reversy.parts.Field;
 import bar.hse.ru.reversy.primitives.Gamer;
 import bar.hse.ru.reversy.primitives.Player;
 import bar.hse.ru.reversy.parts.ScoreBoard;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Game implements Playable{
+public class Game implements Playable {
     private Field gameField;
     private Gamer player1;
     private Gamer player2;
-    private ScoreBoard board;
+    private final ScoreBoard board;
+    private final List<Field> fieldHistory;
+    private int curMove;
+
     public Game() {
         board = new ScoreBoard();
+        fieldHistory = new ArrayList<>();
     }
+
     public void showScoreBoard() {
         this.board.outBoard();
     }
+
     public void showMaxScore() {
         this.board.outBoardMax();
     }
+
     public void runPvPGame() {
         gameField = new Field();
+        fieldHistory.add(gameField.copy());
+        curMove = 1;
         Scanner in = new Scanner(System.in);
         System.out.println("First player:");
         String Name1 = in.nextLine();
@@ -36,43 +48,106 @@ public class Game implements Playable{
         System.out.println("Position - a b");
         System.out.println("a - vertical coordinate");
         System.out.println("b - horizontal coordinate");
+        System.out.println("b - horizontal coordinate");
+        System.out.println("If your move is out of the field, then you have the opportunity to cancel the previous move");
         while (true) {
-            if (canWhiteMove()) {
+            if (canWhiteMove() && (curMove % 2 == 1)) {
                 System.out.println("White move");
                 gameField.outFieldWhite();
                 System.out.println("Enter the position");
-                while (!gameField.whiteMove(in.nextInt(), in.nextInt()));
-            } else if (canNobodyMove()){
+
+                while (true) {
+                    try {
+                        int curInA = in.nextInt();
+                        int curInB = in.nextInt();
+                        in.nextLine();
+                        if (gameField.inField(curInA - 1, curInB - 1)) {
+                            if (gameField.whiteMove(curInA, curInB)) {
+                                ++curMove;
+                                fieldHistory.add(gameField.copy());
+                                break;
+                            }
+                        } else {
+                            System.out.println("Wrong position");
+                            System.out.println("Undo the previous move?(yes/no)");
+                            String answer = in.nextLine();
+                            if (answer.equals("yes")) {
+                                cancelMove(1);
+                                break;
+                            } else {
+                                System.out.println("Enter the position again");
+                            }
+                        }
+                    } catch(Exception e) {
+                        System.out.println("Wrong position");
+                        in.nextLine();
+                    }
+                }
+            } else if (canNobodyMove()) {
                 break;
+            } else if (!canWhiteMove()){
+                ++curMove;
             }
-            if (canBlackMove()) {
+            if (canBlackMove() && (curMove % 2 == 0)) {
                 System.out.println("Black move");
                 gameField.outFieldBlack();
                 System.out.println("Enter the position");
-                while (!gameField.blackMove(in.nextInt(), in.nextInt())) ;
-            } else if (canNobodyMove()){
+                while (true) {
+                    try {
+                        int curInA = in.nextInt();
+                        int curInB = in.nextInt();
+                        in.nextLine();
+                        if (gameField.inField(curInA - 1, curInB - 1)) {
+                            if (gameField.blackMove(curInA, curInB)) {
+                                ++curMove;
+                                fieldHistory.add(gameField.copy());
+                                break;
+                            }
+                        } else {
+                            System.out.println("Wrong position");
+                            System.out.println("Undo the previous move?(yes/no)");
+                            String answer = in.nextLine();
+
+                            if (answer.equals("yes")) {
+                                cancelMove(1);
+                                break;
+                            } else {
+                                System.out.println("Enter the position again");
+                            }
+                        }
+                    } catch(Exception e) {
+                        System.out.println("Wrong position");
+                        in.nextLine();
+                    }
+                }
+            } else if (canNobodyMove()) {
                 break;
+            } else if (!canBlackMove()){
+                ++curMove;
             }
         }
         gameField.outField();
         if (gameField.blackCount() > gameField.whiteCount()) {
             System.out.printf("%s wins\n", this.player2.getName());
-            System.out.printf("%s score: %d\n", player2.getName(),gameField.blackCount());
-            System.out.printf("%s score: %d\n", player1.getName(),gameField.whiteCount());
+            System.out.printf("%s score: %d\n", player2.getName(), gameField.blackCount());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
             this.player2.setScore(gameField.blackCount());
             this.board.addToBoard(this.player2);
         } else if (gameField.blackCount() < gameField.whiteCount()) {
             System.out.printf("%s wins\n", this.player1.getName());
-            System.out.printf("%s score: %d\n", player1.getName(),gameField.whiteCount());
-            System.out.printf("%s score: %d\n", player2.getName(),gameField.blackCount());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
+            System.out.printf("%s score: %d\n", player2.getName(), gameField.blackCount());
             this.player1.setScore(gameField.whiteCount());
             this.board.addToBoard(this.player1);
         } else {
             System.out.println("Draw");
         }
     }
+
     public void runPvEGame() {
         gameField = new Field();
+        fieldHistory.add(gameField.copy());
+        curMove = 1;
         Scanner in = new Scanner(System.in);
         System.out.println("Player name:");
         String Name1 = in.nextLine();
@@ -84,29 +159,57 @@ public class Game implements Playable{
         System.out.println("Position - a b");
         System.out.println("a - vertical coordinate");
         System.out.println("b - horizontal coordinate");
+        System.out.println("If your move is out of the field, then you have the opportunity to cancel the previous move");
         while (true) {
-            if (canWhiteMove()) {
+            if (canWhiteMove() && (curMove % 2 == 1)) {
                 System.out.println("Your move");
                 gameField.outFieldWhite();
                 System.out.println("Enter the position");
-                while (!gameField.whiteMove(in.nextInt(), in.nextInt())) ;
-            } else if (canNobodyMove()){
+                while (true) {
+                    int curInA = in.nextInt();
+                    int curInB = in.nextInt();
+                    in.nextLine();
+                    if (gameField.inField(curInA - 1, curInB - 1)) {
+                        if (gameField.whiteMove(curInA, curInB)) {
+                            ++curMove;
+                            fieldHistory.add(gameField.copy());
+                            break;
+                        }
+                    } else {
+                        System.out.println("Wrong position");
+                        System.out.println("Undo the previous move?(yes/no)");
+                        String answer = in.nextLine();
+                        if (answer.equals("yes")) {
+                            cancelMove(2);
+                            break;
+                        } else {
+                            System.out.println("Enter the position again");
+                        }
+                    }
+                }
+            } else if (canNobodyMove()) {
                 break;
+            } else if (!canWhiteMove()){
+                ++curMove;
             }
-            if (canBlackMove()) {
+            if (canBlackMove() && (curMove % 2 == 0)) {
                 gameField.blackComputerMove();
-            } else if (canNobodyMove()){
+                fieldHistory.add(gameField.copy());
+                ++curMove;
+            } else if (canNobodyMove()) {
                 break;
+            } else if (!canBlackMove()){
+                ++curMove;
             }
         }
         gameField.outField();
         if (gameField.blackCount() > gameField.whiteCount()) {
             System.out.println("Computer wins");
             System.out.printf("Computer score: %d\n", gameField.blackCount());
-            System.out.printf("%s score: %d\n", player1.getName(),gameField.whiteCount());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
         } else if (gameField.blackCount() < gameField.whiteCount()) {
             System.out.printf("%s wins\n", this.player1.getName());
-            System.out.printf("%s score: %d\n", player1.getName(),gameField.whiteCount());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
             System.out.printf("Computer score: %d\n", gameField.blackCount());
             this.player1.setScore(gameField.whiteCount());
             this.board.addToBoard(this.player1);
@@ -114,15 +217,106 @@ public class Game implements Playable{
             System.out.println("Draw");
         }
     }
+
     public void runPvhEGame() {
-        System.out.println("Sorry, there is no realisation here");
+        gameField = new Field();
+        fieldHistory.add(gameField.copy());
+        curMove = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Player name:");
+        String Name1 = in.nextLine();
+        player1 = new Player(Name1);
+        System.out.println("W - white cell");
+        System.out.println("B - black cell");
+        System.out.println("P - cell for possible move");
+        System.out.println("O - empty cell");
+        System.out.println("Position - a b");
+        System.out.println("a - vertical coordinate");
+        System.out.println("b - horizontal coordinate");
+        System.out.println("If your move is out of the field, then you have the opportunity to cancel the previous move");
+        while (true) {
+            if (canWhiteMove() && (curMove % 2 == 1)) {
+                System.out.println("Your move");
+                gameField.outFieldWhite();
+                while (true) {
+                    System.out.println("Enter the position");
+                    int curInA = in.nextInt();
+                    int curInB = in.nextInt();
+                    in.nextLine();
+                    if (gameField.inField(curInA - 1, curInB - 1)) {
+                        if (gameField.whiteMove(curInA, curInB)) {
+                            ++curMove;
+                            fieldHistory.add(gameField.copy());
+                            break;
+                        }
+                    } else {
+                        System.out.println("Wrong position");
+                        System.out.println("Undo the previous move?(yes/no)");
+                        String answer = in.nextLine();
+                        if (answer.equals("yes")) {
+                            cancelMove(2);
+                            break;
+                        } else {
+                            System.out.println("Enter the position again");
+                        }
+                    }
+                }
+            } else if (canNobodyMove()) {
+                break;
+            } else if (!canWhiteMove()) {
+                ++curMove;
+            }
+            if (canBlackMove() && (curMove % 2 == 0)) {
+                if (curMove == 64) {
+                    gameField.blackComputerMove();
+                } else {
+                    gameField.blackHardComputerMove();
+                }
+                fieldHistory.add(gameField.copy());
+                ++curMove;
+            } else if (canNobodyMove()) {
+                break;
+            } else if (!canBlackMove()){
+                ++curMove;
+            }
+        }
+        gameField.outField();
+        if (gameField.blackCount() > gameField.whiteCount()) {
+            System.out.println("Computer wins");
+            System.out.printf("Computer score: %d\n", gameField.blackCount());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
+        } else if (gameField.blackCount() < gameField.whiteCount()) {
+            System.out.printf("%s wins\n", this.player1.getName());
+            System.out.printf("%s score: %d\n", player1.getName(), gameField.whiteCount());
+            System.out.printf("Computer score: %d\n", gameField.blackCount());
+            this.player1.setScore(gameField.whiteCount());
+            this.board.addToBoard(this.player1);
+        } else {
+            System.out.println("Draw");
+        }
     }
+
+    private void cancelMove(int num) {
+        if (fieldHistory.size() >= 2) {
+            gameField = fieldHistory.get(curMove - 1 - num).copy();
+            int newSize = fieldHistory.size() - num;
+            for (int i = fieldHistory.size() - 1; i >= newSize; --i) {
+                fieldHistory.remove(i);
+            }
+            curMove -= num;
+        } else {
+            System.out.println("Sorry, this is first move");
+        }
+    }
+
     private boolean canWhiteMove() {
         return ((gameField.blackCount() + gameField.whiteCount() < 64) && (gameField.blackCount() != 0) && (gameField.whiteCount() != 0) && (gameField.whitePossibleCount() != 0));
     }
+
     private boolean canBlackMove() {
         return ((gameField.blackCount() + gameField.whiteCount() < 64) && (gameField.blackCount() != 0) && (gameField.whiteCount() != 0) && (gameField.blackPossibleCount() != 0));
     }
+
     private boolean canNobodyMove() {
         return ((gameField.blackPossibleCount() == 0) && (gameField.whitePossibleCount() == 0));
     }
